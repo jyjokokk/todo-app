57e1968c797e4b1c292792a9fcdd2a918a7255ec
Jyrki Kokkola <j.j.kokkola@gmail.com>
2024-11-10 03:51:32 +0200

    tsconfig and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,47 @@
 import js from '@eslint/js'
-import ts from '@typescript-eslint/eslint-plugin'
-import tsParser from '@typescript-eslint/parser'
-import prettier from 'eslint-config-prettier'
+// import ts from '@typescript-eslint/eslint-plugin'
+// import tsParser from '@typescript-eslint/parser'
+import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import tseslint from 'typescript-eslint'
 
-export default [
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
   {
-    ...js.configs.recommended,
-    files: ['**/*.ts', '**/*.tsx'],
+    ignores: [
+      'dist',
+      'node_modules',
+      'test/**',
+      '*.spec.ts',
+      '.idea',
+      '.local/',
+      'coverage',
+      '*.json',
+      '*.config.js',
+      'eslint.config.mjs',
+      '.local/*'
+    ],
     languageOptions: {
-      parser: tsParser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module'
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
       }
     },
-    plugins: {
-      '@typescript-eslint': ts
-    },
     rules: {
-      ...ts.configs.recommended.rules,
-      ...prettier.rules
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unnecessary-condition': 'warn',
+      '@typescript-eslint/no-unsafe-member-access': 'warn',
+      '@typescript-eslint/strict-boolean-expressions': 'warn',
+      '@typescript-eslint/no-extraneous-class': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-assignment': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'warn',
+      '@typescript-eslint/no-unsafe-member-access': 'warn',
+      '@typescript-eslint/no-unsafe-return': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/strict-boolean-expressions': 'warn'
     }
   },
-  {
-    ignores: ['dist', 'node_modules', 'test/**', '*.spec.ts', '.idea']
-  }
-]
+  pluginPrettierRecommended
+)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 export function filterNumbers(numbers: number[]): number[] {
   return numbers.filter((number) => number > 0)
 }
+
+export function lol(a: string, b: string): string[] {
+  console.log(`${a} ${b}`)
+  return [b]
+}
+
+const nArr: number[] = [1, 2, 3, 4, 5]
+console.log(filterNumbers(nArr))
